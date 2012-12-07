1285ead6f48da054392bc00ad3f5da2a04f2287b
Samuel Chevet <gw4kfu@gmail.com>
2012-12-08 00:42:42 +0100

    Adding TODO and loader finish

--- Nintendo_NES/nintendo_nes.py
+++ Nintendo_NES/nintendo_nes.py
@@ -4,7 +4,11 @@ import struct
 
 ROM_SIGNATURE_OFFSET 	= 0
 ROM_SIGNATURE 		= "NES\x1A"
+ROM_FORMAT_NAME        	= "Nintendo NES ROM"
 ROM_SIGNATURE_LENGTH 	= 4
+HEADER_SIZE		= 16
+PRG_PAGE_SIZE		= 0x4000
+CHR_PAGE_SIZE		= 0x2000
 RAM_START 		= 0x0
 RAM_SIZE		= 0x2000
 SRAM_START		= 0x6000
@@ -15,6 +19,24 @@ ROM_START		= 0x8000
 ROM_SIZE		= 0x8000
 EXPROM_START		= 0x4020
 EXPROM_SIZE		= 0x1FE0
+
+MAPPER_NONE		= 0
+MAPPER_MMC1		= 1
+MAPPER_UxROM		= 2
+MAPPER_CNROM		= 3
+MAPPER_MMC3		= 4
+MAPPER_MMC5		= 5
+MAPPER_AxROM		= 7
+MAPPER_MMC2		= 9
+MAPPER_MMC4		= 10
+MAPPER_COLOR_DREAMS	= 11
+MAPPER_CPROM		= 13
+MAPPER_BxROM		= 34
+MAPPER_GNROM		= 66
+MAPPER_CAMERICA		= 71
+MAPPER_TLSROM		= 118
+MAPPER_TQROM		= 119
+
 
 def dwordAt(li, off):
 	li.seek(off)
@@ -54,9 +76,9 @@ def load_file(li, neflags, format):
 	idc.RenameSeg(RAM_START, "RAM")
 
 	li.seek(0x6)
-	CartType = struct.unpack("<B", li.read(1))[0]
+	CartTypeLSB = struct.unpack("<B", li.read(1))[0]
 	# SRAM
-	if (((CartType & 0x2) >> 1) == 1):
+	if (((CartTypeLSB & 0x2) >> 1) == 1):
 		idc.AddSeg(SRAM_START, SRAM_START + SRAM_SIZE, 0, 0, idaapi.saRelPara, idaapi.scPub)
 		idc.RenameSeg(SRAM_START, "SRAM")
 
@@ -65,10 +87,62 @@ def load_file(li, neflags, format):
 	idc.RenameSeg(EXPROM_START, "EXPROM")
 
 	# TRAINER
-	if (((CartType & 0x4) >> 2) == 1):
+	if (((CartTypeLSB & 0x4) >> 2) == 1):
 		idc.AddSeg(TRAINER_START, TRAINER_START + TRAINER_SIZE, 0, 0, idaapi.saRelPara, idaapi.scPub)
 		idc.RenameSeg(TRAINER_START, "TRAINER")
 
 	# ROM
 	idc.AddSeg(ROM_START, ROM_START + ROM_SIZE, 0, 0, idaapi.saRelPara, idaapi.scPub)
 	idc.RenameSeg(ROM_START, "ROM")	
+	idc.SetSegmentType(ROM_START, idc.SEG_CODE)
+
+	CartTypeMSB = struct.unpack("<B", li.read(1))[0]
+	mapper_version = (((CartTypeLSB & 0xF0) >> 4) | (CartTypeMSB & 0xF0))
+	if (mapper_version == MAPPER_NONE or
+		mapper_version == MAPPER_MMC1 or
+		mapper_version == MAPPER_UxROM or
+		mapper_version == MAPPER_CNROM or
+		mapper_version == MAPPER_MMC3 or
+		mapper_version == MAPPER_MMC5 or
+		mapper_version == MAPPER_CAMERIC or
+		mapper_version == MAPPER_GNROM):
+		li.seek(0x4)
+		prg_rom_size = struct.unpack("<B", li.read(1))[0]
+		if (prg_rom_size != 0):
+			offset = HEADER_SIZE
+			if (((CartTypeLSB & 0x4) >> 2) == 1):
+				offset += TRAINER_SIZE
+			li.seek(offset)
+			li.file2base(0, ROM_START, ROM_START + PRG_PAGE_SIZE, 0)
+			offset = HEADER_SIZE + (prg_rom_size - 1) * PRG_PAGE_SIZE
+			if (((CartTypeLSB & 0x4) >> 2) == 1):
+				offset += TRAINER_SIZE
+			li.seek(offset)
+			li.file2base(0, ROM_START + PRG_PAGE_SIZE, ROM_START + PRG_PAGE_SIZE + PRG_PAGE_SIZE, 0)
+
+	elif (mapper_version == MAPPER_MMC2):
+		print("Second case mapper")
+	elif (mapper_version == MAPPER_AxROM or
+		mapper_version == MAPPER_COLOR_DREAMS or
+		mapper_version == MAPPER_BxROM):
+		print("Third case mapper")
+	else:
+		print("Mapper %d is not supported" % mapper_version)
+
+	naming()
+	idaapi.add_entry(Word(0xFFFC), Word(0xFFFC), "start", 1)
+	idaapi.cvar.inf.startIP = Word(0xFFFC)
+	idaapi.cvar.inf.beginEA = Word(0xFFFC)	
+
+
+	print("[+] Load OK")
+	return 1
+
+def naming():
+	MakeNameEx(0xFFFA, "NMI_vector", SN_NOCHECK | SN_NOWARN)
+	MakeWord(0xFFFA)
+	MakeNameEx(0xFFFC, "RESET_vector", SN_NOCHECK | SN_NOWARN)
+	MakeWord(0xFFFC)
+	MakeNameEx(0xFFFE, "IRQ_vector", SN_NOCHECK | SN_NOWARN)
+	MakeWord(0xFFFE)
+
