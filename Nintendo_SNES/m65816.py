26f0533d28fa9fe3a95b96be6c9709f48505adad
Samuel Chevet <gw4kfu@gmail.com>
2012-12-10 01:36:14 +0100

    Adding XBA to instru table

--- Nintendo_SNES/m65816.py
+++ Nintendo_SNES/m65816.py
@@ -248,6 +248,8 @@ class m65816_processor_t(idaapi.processor_t):
 		{'name': 'tsb',  'feature': CF_USE1, 'cmt': "Test and set bit"},
 		# WAI
 		{'name': 'wai',  'feature': 0, 'cmt': "Wait for Interupt"},
+		# XBA
+		{'name': 'xba',  'feature': CF_USE1 | CF_USE2, 'cmt': "Exchange B and A 8-bit Accumulators"}
 		# XCE
 		{'name': 'xce',  'feature': CF_USE1 | CF_USE2, 'cmt': "Exchange Carry with emulation"}
 		]
